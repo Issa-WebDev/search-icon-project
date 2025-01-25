71de3fc487db3fce10f7460866ebae4471a6bc73
Issa-WebDev <issawebdev@gmail.com>
2025-01-25 22:24:30 +0100

    hidden search icon

---
+++ src/HiddenSearchBar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+const HiddenSearchBar = () => {
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState("bg-white");
+
+  const handleSearch = () => {
+    setShowInput(true);
+    setBgColor("bg-black");
+  };
+
+  const handleHiddenSearchBar = (e) => {
+    if (e.target.id === "contenair") {
+      setShowInput(false);
+      setBgColor("bg-white");
+    }
+  };
+  return (
+    <div
+      id="contenair"
+      className={`${bgColor} flex items-center justify-center h-screen`}
+      onClick={handleHiddenSearchBar}
+    >
+      {showInput ? (
+        <input
+          type="text"
+          placeholder="Search something..."
+          className="border-orange-400 outline-orange-500 w-[400px] border text-white px-3 py-2 rounded-md placeholder:italic"
+        />
+      ) : (
+        <FaSearch size={20} onClick={handleSearch} className="cursor-pointer" />
+      )}
+    </div>
+  );
+};
+
+export default HiddenSearchBar;
